d41083fd22bec461c755917a3fed6c86654ab1f7
Palzalus <pasm28@outlook.com>
2017-07-13 15:15:38 +0200

    Added jade templates and routing

--- app.js
+++ app.js
@@ -4,10 +4,37 @@ bodyParser = require('body-parser'),
 nodeMailer = require('nodemailer');
 
 let app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', function(req, res) {
+	res.render('index', {title: 'Welcome'});
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+app.get('/contact', function(req, res) {
+	res.render('contact');
+});
+
+app.post('/contact/send', function(req, res) {
+	console.log('form submitted');
+});
 app.listen(3000);
 console.log('Server is running at http://localhost:3000');
+
+
+
+
+
+
+
